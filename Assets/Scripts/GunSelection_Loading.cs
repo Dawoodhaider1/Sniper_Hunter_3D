8a5e9562e75a6948c55d00f9c32f270d04a8dd6f
Dawood Haider <bsf2000647@ue.edu.pk>
2023-03-16 10:37:23 +0500

    Commit 20
    Gun Selection Panel working ...
    Also all the UI scenes are working perfectly...
    Now GamePlay scene is remaining

--- Assets/Scripts/GunSelection_Loading.cs
+++ Assets/Scripts/GunSelection_Loading.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GunSelection_Loading : MonoBehaviour
 {
     public Slider loadingSlider;
     public float loadingTime = 5f;
 
+    private int sceneIndex;
+
     private void Start()
     {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
         StartCoroutine(Loading());
     }
 
@@ -25,8 +29,14 @@ public class GunSelection_Loading : MonoBehaviour
         }
 
         yield return new WaitForSeconds(1.5f);
-
-        Application.LoadLevel("GunSelection");
+        if(sceneIndex == 1)
+        {
+            Application.LoadLevel("GunSelection");
+        }
+        else
+        {
+            SceneManager.LoadScene("GamePlay");
+        }
         // Do something after the loading is complete, such as loading the next scene or enabling the game controls.
     }
 }
